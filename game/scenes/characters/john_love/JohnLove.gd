1abcafb788758b2c3028b6af2d1f0964ab3b2f01
Lucas Rufino Martelotte <45052925+lucas-martelotte@users.noreply.github.com>
2022-01-15 22:08:26 -0300

    chore: organize files

---
+++ game/scenes/characters/john_love/JohnLove.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+const GRAVITY = 0.1
+const SPEED = 5
+
+var velocity = Vector2(0,0)
+
+
+func _physics_process(delta):
+	velocity.x = 0
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		
+	# Gravity loop
+	velocity.y += GRAVITY	
+		
+	self.position += velocity
