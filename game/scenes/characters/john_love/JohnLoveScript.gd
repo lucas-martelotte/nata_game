8f9f364833fb597268a4b3774c315d39d554ac7b
Luís Fernando Garcia Jales <srluisfernando0@gmail.com>
2022-01-21 14:25:35 -0300

    feat add colision and image in john love

--- game/scenes/characters/john_love/JohnLoveScript.gd
+++ game/scenes/characters/john_love/JohnLoveScript.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 
+
 const GRAVITY = 0.1
 const SPEED = 5
 
 var velocity = Vector2(0,0)
+
 
 
 func _physics_process(delta):
@@ -13,8 +15,8 @@ func _physics_process(delta):
 		velocity.x = SPEED	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
-		
+	
 	# Gravity loop
-	velocity.y += GRAVITY	
-		
+	velocity.y += GRAVITY
+	
 	self.position += velocity
